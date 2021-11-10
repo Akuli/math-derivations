5683e58b6e4724a77d635a0e944a9473f592a060
Akuli <akuviljanen17@gmail.com>
2021-11-10 14:19:35 +0200

    inverse matrices

--- build.py
+++ build.py
@@ -204,6 +204,7 @@ def get_sidebar_content(txtfile):
             link("Matrix times number", "linalg/matrix-num"),
             link("Defining span and linear (in)dependence", "linalg/span-and-dep-defs"),
             link("Finding span and checking linear (in)dependence", "linalg/span-and-dep-finding"),
+            link("Inverse matrices", "linalg/matrix-inverse"),
         ])),
     ])
 

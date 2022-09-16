c88aaefc22810f14534b34c9f9399d09e87b88cd
Akuli <akuviljanen17@gmail.com>
2022-09-16 22:38:47 +0300

    animation tweaks

--- js/cross-product.js
+++ js/cross-product.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const WIDTH = 500;
   const HEIGHT = 300;
-  const CORNER_SIZE = 0.3;
-
-  const fixedVector = (new THREE.Vector3(0, 0, 5)).multiplyScalar(0.3);
-  const movingVector = (new THREE.Vector3(5, 0, 5)).multiplyScalar(0.2);
+  const CORNER_SIZE = 0.15;
 
   const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(14, WIDTH / HEIGHT, 1, 10000);
+  const camera = new THREE.PerspectiveCamera(12, WIDTH / HEIGHT, 1, 10000);
   camera.position.set(10, 6, -10);
   camera.lookAt(scene.position);
   camera.updateMatrix();
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
+
   document.getElementById('cross-demo-container').appendChild(renderer.domElement);
 
   function draw90DegreeMark(vector1, vector2) {
@@ -36,11 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xffffff })));
   }
 
-  let previousRenderTime = new Date();
-
   function render() {
     // clear everything
     scene.children.map(object => object.id).forEach(id => scene.remove(scene.getObjectById(id)));
+
+    const fixedVector = (new THREE.Vector3(0, 0, 1)).multiplyScalar(1);
+    const movingVector = (new THREE.Vector3(0, 0, 1)).multiplyScalar(1.5);
+
+    const t = +new Date();
+    movingVector.applyMatrix4(new THREE.Matrix4().makeRotationY((0.0005*t) % (2*Math.PI)));
 
     const crossProduct = (new THREE.Vector3()).copy(fixedVector).cross(movingVector);
     scene.add(commonStuff.createArrow3(fixedVector, 0x6666ff));
@@ -51,11 +53,6 @@ document.addEventListener('DOMContentLoaded', () => {
     ));
     draw90DegreeMark(fixedVector, crossProduct);
     draw90DegreeMark(movingVector, crossProduct);
-
-    const now = new Date();
-    const deltaTime = new Date() - previousRenderTime;
-    previousRenderTime = now;
-    movingVector.applyMatrix4(new THREE.Matrix4().makeRotationY(0.0005 * deltaTime));
 
     renderer.render(scene, camera);
 
